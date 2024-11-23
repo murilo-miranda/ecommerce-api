211ef8358deb70d9acef875e9f767ccc76b49272
Murilo Miranda <murilomirandamoreno@gmail.com>
2024-11-23 15:17:46 -0300

    Route to get cart list
    - Add a new route to get cart info

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
+  get "cart" => "carts#show"
   post "cart/add_items" => "carts#add_items"
   get "up" => "rails/health#show", as: :rails_health_check
 
